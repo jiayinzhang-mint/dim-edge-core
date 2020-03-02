2346c9686c09a62529999a8f3e26d17e6dd143ae
MINT <mint@insdim.com>
2020-03-02 14:01:01 +0800

    feature - get pods info

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module dim-edge-core
 go 1.13
 
 require (
-	github.com/google/martian v2.1.0+incompatible
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/net v0.0.0-20200226051749-491c5fce7268 // indirect
 	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
+	k8s.io/apimachinery v0.17.3
+	k8s.io/client-go v0.17.3
 )
--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "github.com/sirupsen/logrus"
+import (
+	"flag"
+	"os"
+	"path/filepath"
+
+	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
 
 func main() {
-	logrus.Info("DIM Edge service starting")
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	logrus.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	for i := range pods.Items {
+		logrus.Info(pods.Items[i].Name, "  ", pods.Items[i].Status.StartTime)
+	}
+
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
 }
